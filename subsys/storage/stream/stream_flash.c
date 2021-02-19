550120a14d926886c7aa5c05f8b761e678b281d0
Jonathan Nilsen <Jonathan.Nilsen@nordicsemi.no>
2021-02-19 16:11:10 +0100

    NCSDK-7118 possible implementation

--- subsys/storage/stream/stream_flash.c
+++ subsys/storage/stream/stream_flash.c
@@ -16,6 +16,88 @@ LOG_MODULE_REGISTER(LOG_MODULE_NAME, CONFIG_STREAM_FLASH_LOG_LEVEL);
 
 #include <storage/stream_flash.h>
 
+#ifdef CONFIG_STREAM_FLASH_SAVE_PROGRESS
+#include <settings/settings.h>
+
+
+static inline bool progress_enabled(struct stream_flash_ctx *ctx)
+{
+	return ctx->progress_key != NULL;
+}
+
+static int settings_direct_loader(const char *key, size_t len,
+				  settings_read_cb read_cb, void *cb_arg,
+				  void *param)
+{
+	struct stream_flash_ctx *ctx = (struct stream_flash_ctx *) param;
+
+	/* Handle the subtree if it is an exact key match. */
+	if (progress_enabled(ctx) && settings_name_next(key, NULL) == 0) {
+		ssize_t len = read_cb(cb_arg, &ctx->bytes_written,
+				      sizeof(ctx->bytes_written));
+
+		if (len != sizeof(ctx->bytes_written)) {
+			LOG_ERR("Unable to read bytes_written from storage");
+			return len;
+		}
+
+#ifdef CONFIG_STREAM_FLASH_ERASE
+		int rc;
+		struct flash_pages_info page;
+		int abs_offset = ctx->offset + ctx->bytes_written;
+		
+		rc = flash_get_page_info_by_offs(ctx->fdev, abs_offset, &page);
+		if (rc != 0) {
+			LOG_ERR("Error %d while getting page info", rc);
+			return rc;
+		}
+
+		/* Update the last erased page to avoid deleting already
+		 * written data.
+		 */
+		ctx->last_erased_page_start_offset = page.start_offset;
+
+#endif /* CONFIG_STREAM_FLASH_ERASE */
+	}
+
+	return 0;
+}
+
+static int progress_save(struct stream_flash_ctx *ctx)
+{
+	int rc;
+
+	if (!progress_enabled(ctx)) {
+		return -EFAULT;
+	}
+
+	rc = settings_save_one(ctx->progress_key, &ctx->bytes_written,
+		sizeof(ctx->bytes_written));
+	if (rc != 0) {
+		LOG_ERR("Error %d while storing progress", rc);
+	}
+
+	return rc;
+}
+
+static int progress_load(struct stream_flash_ctx *ctx)
+{
+	int rc;
+
+	if (!progress_enabled(ctx)) {
+		return -EFAULT;
+	}
+	rc = settings_load_subtree_direct(ctx->progress_key,
+		settings_direct_loader, (void *) ctx);
+	if (rc != 0) {
+		LOG_ERR("Error %d while loading progress", rc);
+	}
+
+	return rc;
+}
+
+#endif /* CONFIG_STREAM_FLASH_SAVE_PROGRESS */
+
 #ifdef CONFIG_STREAM_FLASH_ERASE
 
 int stream_flash_erase_page(struct stream_flash_ctx *ctx, off_t off)
@@ -173,6 +255,13 @@ int stream_flash_buffered_write(struct stream_flash_ctx *ctx, const uint8_t *dat
 		ctx->bytes_written -= fill_length;
 	}
 
+#ifdef CONFIG_STREAM_FLASH_SAVE_PROGRESS
+	if (rc == 0) {
+		/* FIXME: note to self: error handling */
+		rc = progress_save(ctx);
+	}
+#endif
+
 	return rc;
 }
 
@@ -204,11 +293,19 @@ static bool find_flash_total_size(const struct flash_pages_info *info,
 
 int stream_flash_init(struct stream_flash_ctx *ctx, const struct device *fdev,
 		      uint8_t *buf, size_t buf_len, size_t offset, size_t size,
-		      stream_flash_callback_t cb)
+		      stream_flash_callback_t cb, const char *id)
 {
 	if (!ctx || !fdev || !buf) {
 		return -EFAULT;
 	}
+
+#ifdef CONFIG_STREAM_FLASH_SAVE_PROGRESS
+	int rc = settings_subsys_init();
+	if (rc != 0) {
+		LOG_ERR("settings_subsys_init failed: %d", rc);
+		return rc;
+	}
+#endif
 
 	struct _inspect_flash inspect_flash_ctx = {
 		.buf_len = buf_len,
@@ -248,5 +345,61 @@ int stream_flash_init(struct stream_flash_ctx *ctx, const struct device *fdev,
 	ctx->last_erased_page_start_offset = -1;
 #endif
 
+#ifdef CONFIG_STREAM_FLASH_SAVE_PROGRESS
+	ctx->progress_key = id;
+	/* FIXME: Should we have automatic progress load? */
+	if (id) {
+		return progress_load(ctx);
+	}
+#endif
+
 	return 0;
 }
+
+int stream_flash_finish(struct stream_flash_ctx *ctx, bool flush)
+{
+	int rc = 0;
+
+	if (!ctx ) {
+		return -EFAULT;
+	}
+	if (flush) {
+		rc = stream_flash_buffered_write(ctx, NULL, 0, true);
+		if (rc != 0) {
+			LOG_ERR("Error %d while flushing stream buffer", rc);
+		}
+	}
+
+#ifdef CONFIG_STREAM_FLASH_SAVE_PROGRESS
+	if (!progress_enabled(ctx)) {
+		return 0;
+	}
+	rc = settings_delete(ctx->progress_key);
+	if (rc != 0) {
+		LOG_ERR("Error %d while deleting progress", rc);
+	}
+#endif
+
+	return rc;
+}
+
+/* FIXME: is this API necessary? */
+#ifdef CONFIG_STREAM_FLASH_SAVE_PROGRESS
+
+int stream_flash_progress_load(struct stream_flash_ctx *ctx)
+{
+	if (!ctx) {
+		return -EFAULT;
+	}
+	return progress_load(ctx);
+}
+
+int stream_flash_progress_save(struct stream_flash_ctx *ctx)
+{
+	if (!ctx) {
+		return -EFAULT;
+	}
+	return progress_save(ctx);
+}
+
+#endif  /* CONFIG_STREAM_FLASH_SAVE_PROGRESS */
